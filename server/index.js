873a6da63a2431e5e05ba8d8e27dc80d75f4643c
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-09-23 21:07:00 +0300

    delete server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-
-app.get("/", (req, res) => {
-    res.json({fruits: ["banana", "oranges"]})
-})
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
